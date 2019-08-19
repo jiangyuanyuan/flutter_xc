02965a3426eb13d58a3476ea8d91f044e5bcb35e
jiangyuanyuan <jyy281421465@qq.com>
2019-08-19 22:13:42 +0800

    网格布局

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,89 +3,90 @@ import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:flutter_xc/dao/home_dao.dart';
 import 'package:flutter_xc/model/CommonModel.dart';
+import 'package:flutter_xc/model/GridNavModel.dart';
 import 'package:flutter_xc/model/HomeModel.dart';
 import 'package:flutter_xc/widget/grid_nav.dart';
 import 'package:flutter_xc/widget/local_nav.dart';
 
+const APPBAR_OFFSET = 100;
 
-const APPBAR_OFFSET =100;
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
-
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
   final List _imageList = [
     "https://dimg04.c-ctrip.com/images/350p1600000109qx2C354_C_500_280_Q80.jpg",
     "https://dimg06.c-ctrip.com/images/350r0o000000enmc1BFDC_C_500_280_Q80.jpg"
   ];
   var resultString = "";
   double appBarAlpha = 0;
-  List<CommonModel> localNavList =[];
+  List<CommonModel> localNavList = [];
+  GridNavModel gridNavModel;
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      backgroundColor: Color(0xfff2f2f2),
-      body:Stack(
-        children: <Widget>[
-          MediaQuery.removePadding(
-              removeTop: true,
-              context: context,
-              child:NotificationListener(
-                  onNotification: (scrollNotication){
-                    if(scrollNotication is ScrollUpdateNotification &&scrollNotication.depth==0){
-                      _Scroll(scrollNotication.metrics.pixels);
-                    }
-                  },
-                  child: ListView(
-                    children: <Widget>[
-                      Container(
-                        height: 160,
-                        child: Swiper(itemCount: _imageList.length,
-                          autoplay: true,
-                          itemBuilder: (BuildContext context,int index){
-                            return Image.network(
-                              _imageList[index],
-                              fit: BoxFit.fill,
-                            );
-                          },
-                          pagination: SwiperPagination(),
+        backgroundColor: Color(0xfff2f2f2),
+        body: Stack(
+          children: <Widget>[
+            MediaQuery.removePadding(
+                removeTop: true,
+                context: context,
+                child: NotificationListener(
+                    onNotification: (scrollNotication) {
+                      if (scrollNotication is ScrollUpdateNotification &&
+                          scrollNotication.depth == 0) {
+                        _Scroll(scrollNotication.metrics.pixels);
+                      }
+                    },
+                    child: ListView(
+                      children: <Widget>[
+                        Container(
+                          height: 160,
+                          child: Swiper(
+                            itemCount: _imageList.length,
+                            autoplay: true,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Image.network(
+                                _imageList[index],
+                                fit: BoxFit.fill,
+                              );
+                            },
+                            pagination: SwiperPagination(),
+                          ),
                         ),
-
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
-                        child:  LocalNav(localNavModel: localNavList),
-                      ),
-                      Container(
-                        height: 800,
-                        child: ListTile(title: Text(resultString)),
-                      )
-                    ],
-                  ))
-          ),
-          Opacity(
-            opacity: appBarAlpha,
-            child: Container(
-              height: 80,
-              decoration: BoxDecoration(color: Colors.white),
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: Text("首页"),),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+                          child: LocalNav(localNavModel: localNavList),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+                          child: GridNav(gridNavModel: gridNavModel),
+                        ),
+                        Container(
+                          height: 800,
+                          child: ListTile(title: Text(resultString)),
+                        )
+                      ],
+                    ))),
+            Opacity(
+              opacity: appBarAlpha,
+              child: Container(
+                height: 80,
+                decoration: BoxDecoration(color: Colors.white),
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: Text("首页"),
+                  ),
+                ),
               ),
-            ),
-          )
-        ],
-      )
-
-
-    );
+            )
+          ],
+        ));
   }
-
 
   @override
   void initState() {
@@ -93,26 +94,26 @@ class _HomePageState extends State<HomePage>{
   }
 
   _Scroll(pixels) {
-    double alpha = pixels/APPBAR_OFFSET;
-    if(alpha<0){
+    double alpha = pixels / APPBAR_OFFSET;
+    if (alpha < 0) {
       alpha = 0;
-    }else if (alpha>1){
-      alpha =1;
+    } else if (alpha > 1) {
+      alpha = 1;
     }
     setState(() {
-      appBarAlpha=alpha;
+      appBarAlpha = alpha;
     });
   }
 
-  _loadDate() async{
-    try{
+  _loadDate() async {
+    try {
       HomeModel model = await HomeDao.fetch();
       setState(() {
-        localNavList =model.localNavList;
+        localNavList = model.localNavList;
+        gridNavModel = model.gridNav;
       });
-    }catch(e){
-     print(e);
+    } catch (e) {
+      print(e);
     }
   }
-
 }
--- lib/widget/grid_nav.dart
+++ lib/widget/grid_nav.dart
@@ -1,15 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_xc/model/CommonModel.dart';
 import 'package:flutter_xc/model/GridNavModel.dart';
 
-class GridNav extends StatelessWidget{
+import 'WebViewPage.dart';
+
+class GridNav extends StatelessWidget {
   final GridNavModel gridNavModel;
   final String name;
 
-  const GridNav({Key key, this.gridNavModel, this.name="这是啥"}) : super(key: key);
+  const GridNav({Key key, this.gridNavModel, this.name = "这是啥"})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(name);
+    return PhysicalModel(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.circular(6),
+        clipBehavior: Clip.antiAlias,
+        child: Column(children: _gridNavItems(context)));
+  }
+
+  _gridNavItems(BuildContext context) {
+    List<Widget> itmes = [];
+    if (gridNavModel == null) return itmes;
+    if (gridNavModel.hotel != null) {
+      itmes.add(_gridItem(context, gridNavModel.hotel, true));
+    }
+    if (gridNavModel.flight != null) {
+      itmes.add(_gridItem(context, gridNavModel.flight, false));
+    }
+
+    if (gridNavModel.travel != null) {
+      itmes.add(_gridItem(context, gridNavModel.travel, false));
+    }
+
+    return itmes;
+  }
+
+  _gridItem(BuildContext context, GridNavItem gridNavItem, bool first) {
+    List<Widget> itmes = [];
+    itmes.add(_mainItem(context, gridNavItem.mainItem));
+    itmes.add(_doubleItem(context, gridNavItem.item1, gridNavItem.item2));
+    itmes.add(_doubleItem(context, gridNavItem.item3, gridNavItem.item4));
+    List<Widget> exitmes = [];
+    itmes?.forEach((item) {
+      exitmes.add(Expanded(child: item, flex: 1));
+    });
+    Color startColor = Color(int.parse("0xff" + gridNavItem.startColor));
+    Color endColor = Color(int.parse("0xff" + gridNavItem.endColor));
+
+    return Container(
+      height: 88,
+      margin: first ? null : EdgeInsets.only(top: 3),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [startColor, endColor])),
+      child: Row(children: exitmes),
+    );
+  }
+
+  _mainItem(BuildContext context, CommonModel model) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    WebViewPage(title: model.title, url: model.url)));
+      },
+      child: Stack(
+        alignment: AlignmentDirectional.topCenter,
+        children: <Widget>[
+          Image.network(
+            model.icon,
+            fit: BoxFit.contain,
+            width: 88,
+            height: 121,
+            alignment: AlignmentDirectional.bottomEnd,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 12),
+            child: Text(
+              model.title,
+              style: TextStyle(fontSize: 12, color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _doubleItem(
+      BuildContext context, CommonModel topItem, CommonModel bottomItem) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: _item(context, topItem, true, false),
+        ),
+        Expanded(
+          child: _item(context, bottomItem, false, false),
+        )
+      ],
+    );
+  }
+
+  _item(BuildContext context, CommonModel item, bool first, bool isCenter) {
+    BorderSide borderSide = BorderSide(width: 0.8, color: Colors.white);
+
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: Container(
+          decoration: BoxDecoration(
+              border: Border(
+                  left: borderSide,
+                  bottom: first ? borderSide : BorderSide.none)),
+          child: _wrapGesture(
+              context,
+              Center(
+                child: Text(
+                  item.title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 12, color: Colors.white),
+                ),
+              ),
+              item)),
+    );
+  }
+
+  _wrapGesture(BuildContext context, Widget widget, CommonModel model) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    WebViewPage(title: model.title, url: model.url)));
+      },
+      child: widget,
+    );
   }
 }
-
